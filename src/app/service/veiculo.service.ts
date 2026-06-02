05b1ae5fb1aa81a3a90d50701402dd11fb4abf5c
GustavoSantosMariano <gutavomarianoo@gmail.com>
2026-06-02 15:38:09 -0300

    implementando a veiculo.service.ts

---
+++ src/app/service/veiculo.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Veiculo } from '../models/veiculo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VeiculoService {
+
+  private readonly apiUrl = 'http://localhost:3000/veiculos';
+
+  private readonly http = inject(HttpClient);
+
+  listar(): Observable<Veiculo[]> {
+    return this.http.get<Veiculo[]>(this.apiUrl);
+  }
+
+  criar(veiculo: Veiculo): Observable<Veiculo> {
+    return this.http.post<Veiculo>(this.apiUrl, veiculo);
+  }
+
+  atualizar(id: number | string, veiculo: Veiculo): Observable<Veiculo> {
+    return this.http.put<Veiculo>(
+      `${this.apiUrl}/${id}`,
+      veiculo
+    );
+  }
+
+  excluir(id: number | string): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiUrl}/${id}`
+    );
+  }
+}
